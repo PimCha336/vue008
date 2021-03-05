03257c2bc8db1a7692f7a970069cacaeed11bc24
PimCha336 <75597105+PimCha336@users.noreply.github.com>
2021-03-05 23:52:41 +0700

    Add files via upload

---
+++ src/views/manga.vue
@@ -0,0 +1,39 @@
+<template>
+
+<div>{{toonList.title_english}}<br>
+  <b-img :src="toonList.image_url" fluid alt="Responsive image"></b-img><br>
+  <div class="container">
+  {{toonList.synopsis}}
+  </div>
+</div>
+
+
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+data() {
+    return {
+      toonList: "",
+      url: `https://api.jikan.moe/v3/manga/${this.$route.params.id}`,
+    };
+  },
+  
+  mounted() {
+    axios
+      .get(this.url)
+      .then((response) => {
+        this.toonList = response.data;
+      })
+      .catch((err) => {
+        console.log(err);
+        alert(err);
+      });
+  },
+}
+</script>
+
+<style>
+
+</style>
